083378dd6f912a01fc3904ac1bb54f649faae9d9
Stephen Dickinson <stephencottontail@me.com>
2018-04-24 16:25:32 -0600

    Set up front page loops

---
+++ front-page.php
@@ -0,0 +1,42 @@
+<?php
+
+// Force full width content layout
+add_filter( 'genesis_site_layout', '__genesis_return_full_width_content' );
+
+// We use custom loops on the front page, so we don't need this stuff
+remove_action( 'genesis_loop', 'genesis_do_loop' );
+remove_action( 'genesis_after_endwhile', 'genesis_posts_nav' );
+
+function sc_front_loop() {
+	?>
+	<div class="front-page-posts-section recent-posts-section">
+		<h2 class="section-title">Recent Work</h2>
+		<?php
+		$projects_args = array(
+			'post_type'           => 'jetpack-portfolio',
+			'posts_per_page'      => 2,
+			'ignore_sticky_posts' => true
+		);
+		
+		genesis_custom_loop( $projects_args );
+		?>
+		<a href="<?php esc_url( get_post_type_archive_link( 'jetpack-portfolio' ) ); ?>">See More&hellip;</a>
+	</div>
+	
+	<div class="front-page-posts-section recent-projects-section">
+		<h2 class="section-title">Recent Posts</h2>
+		<?php
+		$blog_args = array(
+			'posts_per_page'      => 2,
+			'ignore_sticky_posts' => true
+		);
+		
+		genesis_custom_loop( $blog_args );
+		?>
+		<a href="<?php esc_url( get_option( 'page_for_posts' ) ); ?>">See More&hellip;</a>
+	</div>
+<?php
+}
+add_action( 'genesis_loop', 'sc_front_loop' );
+	
+genesis();
