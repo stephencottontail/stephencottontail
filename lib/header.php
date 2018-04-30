0a6ffe8a1ecc8bb10416b31eeb7999c95c7e0e41
Stephen Dickinson <stephencottontail@me.com>
2018-04-30 12:56:12 -0600

    Set up home link on the logo

--- lib/header.php
+++ lib/header.php
@@ -3,16 +3,18 @@
 // Add SVG logo
 function sc_svg_logo() {
 	?>
-	<svg class="logo" height="200" width="200" viewBox="0 0 200 200">
-		<linearGradient id="water">
-			<stop class="stop1" offset="0%" />
-			<stop class="stop2" offset="50%" />
-		</linearGradient>
-		<ellipse class="shape expand" cx="57" cy="75" rx="18" ry="60" />
-		<ellipse class="shape expand" cx="143" cy="75" rx="18" ry="60" />
-		<ellipse class="shape expand" cx="100" cy="140" rx="85" ry="55" stroke-width="3" />
-		<text class="letter" text-anchor="middle" x="100" y="175">S</text>
-	</svg>
+	<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="site-home-link">
+		<svg class="logo" height="200" width="200" viewBox="0 0 200 200">
+			<linearGradient id="water">
+				<stop class="stop1" offset="0%" />
+				<stop class="stop2" offset="50%" />
+			</linearGradient>
+			<ellipse class="shape expand" cx="57" cy="75" rx="18" ry="60" />
+			<ellipse class="shape expand" cx="143" cy="75" rx="18" ry="60" />
+			<ellipse class="shape expand" cx="100" cy="140" rx="85" ry="55" stroke-width="3" />
+			<text class="letter" text-anchor="middle" x="100" y="175">S</text>
+		</svg>
+	</a>
 <?php
 }
 add_action( 'genesis_header', 'sc_svg_logo' );
